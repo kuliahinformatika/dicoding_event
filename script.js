210e7179a2847c1a39a0db1e4bbec3bcde75b7ad
kuliahinformatika <murodalhakim01@gmail.com>
2025-10-07 19:53:36 +0700

    MENARUNYA KE INLINE EVENNYA

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 function increment() {
     document.getElementById('count').innerText++;
-   
+
     if (document.getElementById('count').innerText == 3) {
-      const hiddenMessage = document.createElement('h4');
-      hiddenMessage.innerText = 'Selamat! Anda menemukan hadiah tersembunyi...';
- 
-      const image = document.createElement('img');
-      image.setAttribute(
-        'src',
-        'https://raw.githubusercontent.com/dicodingacademy/a315-web-pemula-labs/shared-files/catto.jpg',
-      );
- 
-      const contents = document.querySelector('.contents');
-      contents.appendChild(hiddenMessage).appendChild(image);
+        const hiddenMessage = document.createElement('h4');
+        hiddenMessage.innerText = 'Selamat! Anda menemukan hadiah tersembunyi...';
+
+        const image = document.createElement('img');
+        image.setAttribute(
+            'src',
+            'https://raw.githubusercontent.com/dicodingacademy/a315-web-pemula-labs/shared-files/catto.jpg',
+        );
+
+        const contents = document.querySelector('.contents');
+        contents.appendChild(hiddenMessage).appendChild(image);
     }
-  }
- 
-  function welcome() {
+}
+
+function welcome() {
     alert('Sim salabim muncullah elemen-elemen HTML!');
- 
+
     const contents = document.querySelector('.contents');
     contents.removeAttribute('hidden');
-  }
- 
-  document.getElementById('incrementButton').onclick = increment;
-  document.body.onload = welcome;
-  
+}
+
+// document.body.onload = welcome;
+// document.getElementById('incrementButton').onclick = increment;
+
+
